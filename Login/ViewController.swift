9b065d16fe8aa0d57ce9d2b12c7832db2c1a59e8
MrMurman <storm2150@yandex.ru>
2021-06-27 17:10:44 +0300

    v 1- better understanding of segues

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var forgotUserBTN: UIButton!
+    @IBOutlet var forgotPassBTN: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let sender = sender as? UIButton else {return}
+        
+        if sender == forgotUserBTN {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else if sender == forgotPassBTN {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else {segue.destination.navigationItem.title = usernameTextField.text }
+    }
 
+    @IBAction func forgotUsernameButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "GeneralSegue", sender: sender)
+        
+    }
+    @IBAction func forgotPasswordButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "GeneralSegue", sender: sender)
+    }
+    
 }
 
